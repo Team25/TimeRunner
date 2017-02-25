d82cf652ad8a0885b5feb444b9f1a9455bd6f50e
Wolfcoder13 <Wolfninja13@gmail.com>
2017-02-25 13:18:43 +0000

    Naming conventions might be off. Testing a commit No functionality yet

--- app/src/main/java/com/t25/hbv601g/timerunner/LoginActivity.java
+++ app/src/main/java/com/t25/hbv601g/timerunner/LoginActivity.java
@@ -2,12 +2,35 @@ package com.t25.hbv601g.timerunner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button mLoginButton;
+    EditText mUsername;
+    EditText mPassword;
+    TextView mForgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mLoginButton = (Button) findViewById(R.id.login_button);
+        mUsername = (EditText) findViewById(R.id.input_login_username);
+        mPassword = (EditText) findViewById(R.id.input_login_password);
+        mForgotPassword = (TextView) findViewById(R.id.text_login_reclaimPassword);
+
+        mForgotPassword.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                // new activity for this?
+            }
+        });
+
+
     }
 }
