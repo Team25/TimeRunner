0618042b31101830c7adc11dde751dc203495a2a
Wolfcoder13 <Wolfninja13@gmail.com>
2017-03-13 00:48:09 +0000

    Added emtpy clock activity

---
+++ app/src/main/java/com/t25/hbv601g/timerunner/ClockActivity.java
@@ -0,0 +1,13 @@
+package com.t25.hbv601g.timerunner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock);
+    }
+}
