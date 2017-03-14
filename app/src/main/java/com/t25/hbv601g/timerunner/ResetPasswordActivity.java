cc93abd4232ac8ff00c0ae85f8ea8dcbb80afa88
Vitringur <pgi1@hi.is>
2017-03-14 21:20:39 +0000

    bætti við grunn að resetpasssww

---
+++ app/src/main/java/com/t25/hbv601g/timerunner/ResetPasswordActivity.java
@@ -0,0 +1,28 @@
+package com.t25.hbv601g.timerunner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    Button mResetButton;
+    EditText mUserName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+
+        mResetButton = (Button) findViewById(R.id.reset_button);
+        mResetButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //TODO
+            }
+        });
+
+    }
+}
