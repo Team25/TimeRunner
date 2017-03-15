67a6b2553e6110d2ce0a4937370d23784ec2abcb
Ólafur Ingi Guðmundsson <lafur Ingi Gumundsson>
2017-03-15 13:33:49 +0000

    Logo test. setti liti til að testa með logo. fullscreen og meira awesomeness

---
+++ app/src/main/java/com/t25/hbv601g/timerunner/WifiChangeReceiver.java
@@ -0,0 +1,27 @@
+package com.t25.hbv601g.timerunner;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.widget.Toast;
+
+public class WifiChangeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NetworkInfo info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+        if (info != null && info.isConnected()) {
+            WifiManager wifiManager = (WifiManager)context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            if (wifiInfo.getSSID().equals("Vinna")) {
+                Toast.makeText(context, "Connected to Vinna. ALERT AWAY", Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(context, "Connected to something else. Boring.", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+}
