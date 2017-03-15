fd503836626c0f33639483c132161a8c88d53743
Örn Arnarson <orn@arnarson.net>
2017-03-15 00:00:07 +0000

    BroadcastReceiver work in progress. Work from Wifi names instead of static strings. Will need to test on phone as wifi emulation not available.

--- app/src/main/java/com/t25/hbv601g/timerunner/ReminderReceiver.java
+++ app/src/main/java/com/t25/hbv601g/timerunner/ReminderReceiver.java
@@ -3,6 +3,9 @@ package com.t25.hbv601g.timerunner;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.widget.Toast;
 
 /**
@@ -11,7 +14,18 @@ import android.widget.Toast;
 
 public class ReminderReceiver extends BroadcastReceiver {
     @Override
-    public void onReceive(Context ctx, Intent i) {
-        Toast.makeText(ctx, "Test Receiver says hello", Toast.LENGTH_LONG).show();
+    public void onReceive(Context context, Intent intent) {
+        NetworkInfo info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+        if (info != null && info.isConnected()) {
+            WifiManager wifiManager = (WifiManager)context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            if (wifiInfo.getSSID().equals("Vinna")) {
+                Toast.makeText(context, "Connected to Vinna. ALERT AWAY", Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(context, "Connected to something else. Boring.", Toast.LENGTH_LONG).show();
+            }
+        }
+
     }
 }
